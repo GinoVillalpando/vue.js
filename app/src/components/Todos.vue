33f85d600a20751b02e35971586ddf1309432d0c
Gino Villalpando <grievxus@outlook.com>
2020-06-20 23:30:08 -0600

    created a todo component that takes the todo data from app.vue and renders it in it's own component

--- app/src/components/Todos.vue
+++ app/src/components/Todos.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div v-bind:key="todo.id" v-for="todo in todos">
-        <h1>{{ todo.title }}</h1>
+            <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+                <h1>{{ todo.title }}</h1>
+            </div>
         </div>
     </div>
 </template>
@@ -14,5 +16,13 @@ export default {
 </script>
 
 <style scoped>
+    .todo-item {
+        background: #333;
+        padding: 2rem;
+        border-bottom: 1px black dashed;
+    }
+    .is-complete {
+        text-decoration: line-through;
+    }
 
 </style>
