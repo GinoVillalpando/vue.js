a6d8df61fc7cce191d86ad68c8df73c4d0f69389
Gino Villalpando <grievxus@outlook.com>
2020-07-01 21:28:18 -0600

    added todo app with navbar

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,69 @@
 <template>
   <v-app>
-    <Header msg="Home"/>
+    <Header />
     <v-main>
-      <HelloWorld/>
+      <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-import Header from './components/Header'
-
+import Header from './components/Header.vue'
+import Todos from './components/Todos.vue'
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
-    Header
+    Header,
+    Todos
   },
-
   data: () => ({
-    //
+      todos: [
+        {
+          id: 1,
+          title: "wash the car",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "wash the dishes",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "walk cloudadawa",
+          completed: false
+        }
+        ]
   }),
-};
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(
+        todo => todo.id !== id
+      )
+    }
+  }
+}
 </script>
+
+<style>
+ * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+</style>
