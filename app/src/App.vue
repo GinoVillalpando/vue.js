c9fc8e11874fbe8a38eb536e829d10fe5f053782
Gino Villalpando <grievxus@outlook.com>
2020-07-01 21:16:58 -0600

    added vuetify to vue app and messed with creating a nav bar

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,70 +1,26 @@
 <template>
-  <div id="app">
-    <Header msg="To-Do App" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
-  </div>
+  <v-app>
+    <Header msg="Home"/>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Todos from './components/Todos.vue'
+import HelloWorld from './components/HelloWorld';
+import Header from './components/Header'
 
 export default {
   name: 'App',
-  components: {
-    Header,
-    Todos
-  },
-  data() {
-    return{
-      todos: [
-        {
-          id: 1,
-          title: "wash the car",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "wash the dishes",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "walk cloudadawa",
-          completed: false
-        }
-        ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(
-        todo => todo.id !== id
-      )
-    }
-  }
-}
-</script>
 
-<style>
- * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover {
-    background: #666;
-  }
-</style>
+  components: {
+    HelloWorld,
+    Header
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="header">
-    <h1>{{ msg }}</h1>
-    <h2>{{ about }}</h2>
-  </div>
+    <v-app-bar max-height="100%" :absolute="true" dark fixed app>
+        <v-toolbar-title>Hi</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-menu>
+          <template v-slot:activator="{ on, attrs }">
+            <v-app-bar-nav-icon v-bind="attrs" v-on="on" slot="activator" />
+          </template>
+          <v-list>
+            <v-list-item v-for="item in menu" :key="item.id" :to="item.link">
+              <v-list-item-title>{{ item.name }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+    </v-app-bar>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    menu: [
+      {
+        id: 1,
+        name: "home",
+        link: "home"
+      },
+      {
+        id: 2,
+        name: "social",
+        link: "social"
+      },
+      {
+        id: 3,
+        name: "blog",
+        link: "blog"
+      },
+    ]
+  }),
   name: 'Header',
   props: {
     msg: String,
@@ -17,11 +46,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .header {
-    background-color: #333;
-    margin-bottom: 2rem;
-    padding: 1.5rem;
-    display: flex;
-    justify-content: center;
-  }
+
 </style>
